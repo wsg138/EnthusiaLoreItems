97bfc340ddc8360a8342262156821b376fe27da2
P2wn <racecarboy77@gmail.com>
2026-08-02 18:43:31 -0400

    build: make plugin resources configuration-cache safe

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,17 +2,21 @@ plugins {
     id("com.gradleup.shadow")
 }
 
+val pluginVersion = project.version.toString()
+
 dependencies {
     implementation(project(":api"))
     implementation(project(":adapters-paper"))
     implementation(project(":adapters-sqlite"))
+    implementation("org.snakeyaml:snakeyaml-engine:2.9")
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
 }
 
 tasks {
     processResources {
+        inputs.property("version", pluginVersion)
         filesMatching("plugin.yml") {
-            expand("version" to project.version)
+            expand("version" to pluginVersion)
         }
     }
     shadowJar {
