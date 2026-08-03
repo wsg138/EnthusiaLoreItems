8d36c4870c9f32952c9216e784dae2e647e623bd
P2wn <racecarboy77@gmail.com>
2026-08-03 18:44:49 -0400

    preserve control characters in void audit JSON

--- adapters-sqlite/src/main/java/net/enthusia/loreitems/sqlite/SQLiteVoidLossStore.java
+++ adapters-sqlite/src/main/java/net/enthusia/loreitems/sqlite/SQLiteVoidLossStore.java
@@ -441,11 +441,25 @@ public final class SQLiteVoidLossStore implements VoidLossStore {
     }
 
     private static String jsonEscape(String value) {
-        return value.replace("\\", "\\\\")
-                .replace("\"", "\\\"")
-                .replace("\n", "\\n")
-                .replace("\r", "\\r")
-                .replace("\t", "\\t");
+        StringBuilder escaped = new StringBuilder(value.length() + 16);
+        for (int index = 0; index < value.length(); index++) {
+            char character = value.charAt(index);
+            switch (character) {
+                case '\\' -> escaped.append("\\\\");
+                case '"' -> escaped.append("\\\"");
+                case '\n' -> escaped.append("\\n");
+                case '\r' -> escaped.append("\\r");
+                case '\t' -> escaped.append("\\t");
+                default -> {
+                    if (character < 0x20) {
+                        escaped.append(String.format("\\u%04x", (int) character));
+                    } else {
+                        escaped.append(character);
+                    }
+                }
+            }
+        }
+        return escaped.toString();
     }
 
     private record InstanceRow(
