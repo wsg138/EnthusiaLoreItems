84db91a7268a2fb9fe5122f8fa119d5839434acf
P2wn <racecarboy77@gmail.com>
2026-08-08 02:07:30 -0400

    WP-05: register adopted-location migration

--- adapters-sqlite/src/main/java/net/enthusia/loreitems/sqlite/MigrationRunner.java
+++ adapters-sqlite/src/main/java/net/enthusia/loreitems/sqlite/MigrationRunner.java
@@ -20,7 +20,8 @@ public final class MigrationRunner {
             new Migration(4, "template editor confirmations", "db/migration/V4__template_editor_confirmations.sql"),
             new Migration(5, "destructive administration", "db/migration/V5__destructive_administration.sql"),
             new Migration(6, "mass distribution recipient states", "db/migration/V6__mass_distribution_recipient_states.sql"),
-            new Migration(7, "mass distribution revision snapshot", "db/migration/V7__mass_distribution_revision_snapshot.sql"));
+            new Migration(7, "mass distribution revision snapshot", "db/migration/V7__mass_distribution_revision_snapshot.sql"),
+            new Migration(8, "canonicalize adopted player locations", "db/migration/V8__canonicalize_adopted_player_locations.sql"));
     private static final int LATEST_SCHEMA_VERSION = MIGRATIONS.getLast().version();
 
     public void migrate(Connection connection) throws SQLException {
